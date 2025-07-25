5b2e2590700f00dddb59f65f77b71de5f94a8cdd
Antipoison7 <75201302+Antipoison7@users.noreply.github.com>
2025-07-25 15:19:55 +1000

    Feature: Added basic login features

--- site/resources/components/validation/validate.php
+++ site/resources/components/validation/validate.php
@@ -1,7 +1,24 @@
 <?php
+    include_once("./resources/components/validation/userCred.php");
+
     function validateInputs(string $title, string $category, string $description, string $content){
         if(!strlen($title) || !strlen($category) || !strlen($description) || !strlen($content)){
             return false;
+        }
+    }
+
+    function checkPassword(string $username, string $password){
+        global $adminPassword, $adminUsername;
+        try{
+            if(($username == $adminUsername)&&(password_verify($password, $adminPassword))){
+                return(true);
+            }
+            else{
+                return(false);
+            }
+        }
+        catch(Exception $e){
+            return(false);
         }
     }
 ?>
